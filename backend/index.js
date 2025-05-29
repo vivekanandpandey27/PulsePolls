16c0178fbf696cd8fc8d6c2c0f83d5aa90f85125
vivekanandpandey27 <Vivekanandpdy732@gmail.com>
2025-05-29 21:26:27 +0530

    Database Connection Code
    Completed Database Connection Code

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const dotenv = require('dotenv');
 const { connect } = require('mongoose');
 const cors = require('cors');
-var cookieParser = require('cookie-parser')
-
+//var cookieParser = require('cookie-parser')
+const dbConnect = require('./config/database')
 dotenv.config();
+
+
+const app = express();
 
 
 const PORT = process.env.PORT || 5000;
@@ -17,17 +20,18 @@ const corsOption= {
 
 //middleware to parse upcoming request
 app.use(express.json());
-app.use(cookieParser());
+//app.use(cookieParser());
 app.use(express.urlencoded({ extended: true })); 
 app.use(cors(corsOption));
 
 //routes or mounting url
-app.use("api/v1/user",userRoutes);
-app.use("api/v1/polls",PollRequests);
+// app.use("api/v1/user",userRoutes);
+// app.use("api/v1/polls",PollRequests);
 
-server.listen(PORT,()=>{
-    console.log(`Server listen at port ${PORT}`);
-})
+const server = app.listen(PORT, () => {
+  console.log("Server running...");
+});
+
 
 dbConnect();
 
