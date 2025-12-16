3b4222983ad447d931ccfb8f83a987cef92e9e3f
Samar Mishra <153423012+SamMish45@users.noreply.github.com>
2025-12-16 10:26:25 +0530

    Add comments to userSlice actions and reducer
    Added comments to clarify the purpose of exported actions and reducer.

--- frontend/src/redux/userSlice.js
+++ frontend/src/redux/userSlice.js
@@ -12,5 +12,6 @@ const userSlice = createSlice({
     }
 });
 
-export const{setAuthUser} = userSlice.actions;
-export default userSlice.reducer; 
+export const{setAuthUser} = userSlice.actions; //Kyu: Taaki Frontend Components (Login Page) is action ko use kar sakein. (Courier bhejne ke liye).
+export default userSlice.reducer; //Kyu: Taaki Store (store.js) is slice ko apne andar register kar sake. (Manager ko office mein bithane ke liye).
+
